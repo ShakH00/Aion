f182d3c2ef31b4283b59afc09b0a301ec858858b
Abdulrahman Negmeldin <abdulrahman.m.negmeldin@gmail.com>
2026-02-14 20:22:42 -0700

    basic changes to make helper stuff

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
-from flask import Flask, send_from_directory
+from flask import Flask, send_from_directory, session
 from pathlib import Path
-from user import user
+import users
+from users import user
+from pymongo import MongoClient
+import os
 
+client = MongoClient(os.getenv("DB_KEY"))
+db = client['aionDB']
+user_c = db['users']
 app = Flask(__name__)
 INDEX_DIR = Path(app.root_path) / "index"
 
@@ -10,6 +16,15 @@ INDEX_DIR = Path(app.root_path) / "index"
 def index():
     return send_from_directory(INDEX_DIR, "index.html")
 
+
+#helper method to get the username (first and last name) of an account
+def getUserName():
+    all_users = users.get_all_users()
+    userName = ""
+    for user in all_users:
+        if user['email'] == session['email']:
+            userName += f"{user['name']} {user['lastname']}"
+    return userName
 
 if __name__ == "__main__":
 	app.run(debug=True)
